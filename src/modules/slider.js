6a2ad6586f029c41c432cd944b1a06377c2eca3f
Tevikansky <valerabogdanov@inbox.ru>
2023-01-16 22:44:24 +0600

    add first slider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,5 +1,16 @@
-const slider = () => {
+import Swiper, {Autoplay} from "swiper"
 
+const slider = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    loop: true,
+    modules: [Autoplay],
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }
+  })
 }
 
 export default slider
