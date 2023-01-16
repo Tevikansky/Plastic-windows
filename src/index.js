221f9fc8b2dc35f75bbc4fb1d066e0adf873d754
Tevikansky <valerabogdanov@inbox.ru>
2023-01-16 19:39:07 +0600

    add animation for modal windows

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import modal from "./modules/modal";
-
+import slider from "./modules/slider";
 
 
 modal()
+slider()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,18 +1,72 @@
+import {
+  animate
+} from "./helpers";
+
 const modal = () => {
   const header = document.getElementById('header')
   const headerModal = document.querySelector('.header-modal')
+  const servicesModal = document.querySelector('.services-modal')
   const overlay = document.querySelector('.overlay')
+  const services = document.getElementById('services')
+
+  const blockBody = () => {
+    const body = document.body;
+    body.style.overflow = 'hidden';
+    const bodyScroll = calcScroll();
+    body.style.marginRight = `${bodyScroll}px`;
+  }
+  const unBlockBody = () => {
+    const body = document.body;
+    body.style.overflow = 'auto';
+    body.style.marginRight = `0`;
+  }
+  const calcScroll = () => {
+    let div = document.createElement('div');
+    div.style.width = '500px';
+    div.style.height = '500px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
+  const modalAnimate = (e) => {
+    e.style.left = 0
+    e.style.transform = `transform: translateX(-50%);`
+    e.style.display = "flex"
+    animate({
+      duration: 700,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        e.style.left = progress * 50 + '%';
+      }
+    });
+    blockBody();
+  }
+
 
   header.addEventListener('click', (e) => {
     if (e.target.closest('.fancyboxModal')) {
-      headerModal.style.display = "flex"
       overlay.style.display = "flex"
+      modalAnimate(headerModal)
     }
   })
   document.body.addEventListener('click', (e) => {
-    if (e.target.closest('.header-modal__close') || e.target.closest('.overlay')) {
+    if (e.target.closest('.header-modal__close') || e.target.closest('.overlay') || e.target.closest('.services-modal__close')) {
+      unBlockBody();
       headerModal.style.display = "none"
+      servicesModal.style.display = "none"
       overlay.style.display = "none"
+    }
+  })
+  services.addEventListener('click', (e) => {
+    if (e.target.closest('.service-button')) {
+      overlay.style.display = "flex"
+      modalAnimate(servicesModal)
+
     }
   })
 
